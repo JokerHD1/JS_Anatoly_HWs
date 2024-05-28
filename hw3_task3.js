3e952d686864610977eeaadbaf034d19105890a0
Igor Nikonov <grafmontecristo007@yandex.ru>
2024-05-28 19:28:51 +0300

    let to const

--- hw3_task3.js
+++ hw3_task3.js
@@ -1,11 +1,11 @@
 function calculateSum() {
-    let n = document.getElementById('numberInput').value;
+    const n = document.getElementById('numberInput').value;
     
     if (n >= 1 && n <= 9) {
-        let nAsString = n.toString();
-        let nn = nAsString + nAsString;
-        let nnn = nAsString + nAsString + nAsString;
-        let sum = parseInt(n, 10) + parseInt(nn, 10) + parseInt(nnn, 10);
+        const nAsString = n.toString();
+        const nn = nAsString + nAsString;
+        const nnn = nAsString + nAsString + nAsString;
+        const sum = parseInt(n, 10) + parseInt(nn, 10) + parseInt(nnn, 10);
         alert(`Сумма равна: ${sum}`);
     } else {
         alert("Введено некорректное число. Пожалуйста, введите целое положительное число от 1 до 9.");
